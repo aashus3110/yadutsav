33c10f99dd850ea7ff344938fe1c86c4f84654d0
ashu3110 <avsingh000.789@gmail.com>
2023-11-21 12:56:17 +0530

    nd

--- src/components/ServicesC.jsx
+++ src/components/ServicesC.jsx
@@ -8,18 +8,18 @@ const ServicesC = () => {
         <motion.div
           whileInView={{ x: 0, scale: [0, 1] }}
           initial={{ x: 0, scale: 0 }}
-          animate={{ x: [-100, 0], scale: [null, 0.5, 1] }}
+          animate={{ x: [-100, 0], scale: [null, 1] }}
           transition={{ ease: "easeIn", duration: 3 }}
-          className="md:text-center text-start pt-5 md:text-5xl text-2xl md:px-20 px-5 font-black md:font-black capitalize bg-gradient-to-tr from-secondary from-11% via-primary via-41% to-accent to-21% bg-clip-text text-transparent"
+          className="md:text-center text-start pt-5 md:text-5xl text-xl -tracking-wider md:px-20 pl-5 overflow-x-hidden font-black md:font-black capitalize bg-gradient-to-tr from-secondary from-11% via-primary via-41% to-accent to-21% bg-clip-text text-transparent"
         >
           AYODHYA, UTTAR PRADESH EVENT PLANNER:
         </motion.div>
         <motion.div
           whileInView={{ x: 0, scale: [0, 1] }}
           initial={{ x: 0, scale: 0 }}
-          animate={{ x: [-100, 0], scale: [null, 0.5, 1] }}
+          animate={{ x: [-100, 0], scale: [null, 1] }}
           transition={{ ease: "easeIn", duration: 3, delay: 2 }}
-          className="md:text-center text-start pb-5 md:text-5xl text-2xl md:px-20 px-5 font-black md:font-black capitalize bg-gradient-to-tl from-accent from-11% via-primary via-41% to-secondary to-21% bg-clip-text text-transparent"
+          className="md:text-center text-start pb-5 md:text-5xl text-xl -tracking-wider md:px-20 pl-5 ovxh font-black md:font-black capitalize bg-gradient-to-tl from-accent from-11% via-primary via-41% to-secondary to-21% bg-clip-text text-transparent"
         >
           CREATING TIMELESS MEMORIES FOR YOUR SPECIAL OCCASION
         </motion.div>
